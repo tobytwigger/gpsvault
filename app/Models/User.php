e1623da431a8d680828b0e62c83a72d7a0ec12a4
Toby Twigger <tobytwigger1@gmail.com>
2021-12-13 03:10:09 +0000

    Add deployer support and make sure right filesystem is used

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use App\Integrations\Strava\StravaToken;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\App;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
@@ -91,7 +92,7 @@ class User extends Authenticatable
 
     public function disk()
     {
-        return 'local';
+        return App::isLocal() ? 'local' : 's3';
     }
 
     public function syncs()
---
+++ deploy.php
@@ -0,0 +1,33 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/php-fpm.php';
+
+// Config
+
+set('repository', 'git@github.com:tobytwigger/cycle-store');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('cycle.linkeys.app')
+    ->setSshMultiplexing(true)
+    ->set('remote_user', 'ubuntu')
+    ->set('deploy_path', '/var/www/cycle.linkeys.app');
+
+// Tasks
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'deploy:publish'
+]);
+
+after('deploy:failed', 'deploy:unlock');
