a776081d08f674745f227ed9f317b8191b35ff2c
tobytwigger <tobytwigger@users.noreply.github.com>
2022-07-23 16:46:18 +0000

    Apply php-cs-fixer changes

--- tests/Feature/Auth/SanctumAuthTest.php
+++ tests/Feature/Auth/SanctumAuthTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -12,8 +10,7 @@ class SanctumAuthTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function unfinished(){
-
+    public function unfinished()
+    {
     }
-
 }
