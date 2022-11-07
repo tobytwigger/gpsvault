15e954e92559fef254dad92effd59668085c3b4e
Toby Twigger <tobytwigger1@gmail.com>
2022-11-07 23:03:08 +0000

    Add docblocks to analyser

--- app/Services/Analysis/Analyser/Analyser.php
+++ app/Services/Analysis/Analyser/Analyser.php
@@ -2,8 +2,14 @@
 
 namespace App\Services\Analysis\Analyser;
 
+use App\Models\File;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static Analysis analyse(File $file) Analyse a file
+ * @method static Analysis runAnalysis(Analysis $analysis) Run an analysis on an existing analysis model
+ * @method static AnalysisFactoryContract registerAnalyser(string $class) Register a new analyser type
+ */
 class Analyser extends Facade
 {
     protected static function getFacadeAccessor()
