064b36340a842880d613e4aeeb2f9a333275744b
Toby Twigger <tobytwigger1@gmail.com>
2022-11-09 00:03:25 +0000

    Add IDE helper and add user_id to waypoint model

--- app/Integrations/Strava/Import/Upload/Models/StravaImport.php
+++ app/Integrations/Strava/Import/Upload/Models/StravaImport.php
@@ -5,6 +5,24 @@ namespace App\Integrations\Strava\Import\Upload\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * App\Integrations\Strava\Import\Upload\Models\StravaImport
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Integrations\Strava\Import\Upload\Models\StravaImportResult[] $stravaImportResults
+ * @property-read int|null $strava_import_results_count
+ * @method static \Illuminate\Database\Eloquent\Builder|StravaImport newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|StravaImport newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|StravaImport query()
+ * @method static \Illuminate\Database\Eloquent\Builder|StravaImport whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StravaImport whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StravaImport whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StravaImport whereUserId($value)
+ * @mixin \Eloquent
+ */
 class StravaImport extends Model
 {
     protected $fillable = ['user_id'];
--- app/Models/PlaceRoute.php
+++ app/Models/PlaceRoute.php
@@ -4,6 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * App\Models\PlaceRoute
+ *
+ * @property int $id
+ * @property int $route_id
+ * @property int $place_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute wherePlaceId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute whereRouteId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PlaceRoute whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class PlaceRoute extends Pivot
 {
     public $incrementing = true;
