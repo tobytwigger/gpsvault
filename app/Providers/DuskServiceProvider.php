40706f32b5a30174bad09b3ea91de13154a47d29
Toby Twigger <tobytwigger1@gmail.com>
2022-11-19 02:10:45 +0000

    Fix issue with dusk browser class not existing

--- app/Providers/DuskServiceProvider.php
+++ app/Providers/DuskServiceProvider.php
@@ -10,10 +10,12 @@ class DuskServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        Browser::macro('docsScreenshot', function (string $name) {
-            $this->responsiveScreenshots('../../../resources/images/documentation/' . $name);
-            return $this;
-        });
+        if(class_exists(Browser::class)) {
+            Browser::macro('docsScreenshot', function (string $name) {
+                $this->responsiveScreenshots('../../../resources/images/documentation/' . $name);
+                return $this;
+            });
+        }
     }
 
 }
