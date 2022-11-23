af8c9243cf36c05bd1decbca131897a40d98462b
Toby Twigger <toby.twigger@swytchbike.com>
2022-11-23 19:32:25 +0000

    Fix bug on contact screen

--- resources/js/pages/Public/Contact.vue
+++ resources/js/pages/Public/Contact.vue
@@ -15,7 +15,7 @@
                             class="text-center"
                             cols="12"
                         >
-                                class="text-h2 font-weight-bold black--text mb-4"
+                            <h1 class="text-h2 font-weight-bold black--text mb-4">
                                 Contact Us
                             </h1>
                             <v-btn
