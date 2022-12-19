8541b58bf3c08dc41975df81796f2b9099f01170
Toby Twigger <tobytwigger1@gmail.com>
2022-12-19 17:26:52 +0000

    Remove comments

--- resources/js/ui/components/Route/CRouteTimeline.vue
+++ resources/js/ui/components/Route/CRouteTimeline.vue
@@ -13,49 +13,6 @@
                 :route-distance="routeModel.path.distance"></c-route-timeline-card>
 
         </v-timeline>
-        <!--                <v-row-->
-        <!--                    align="center"-->
-        <!--                    justify="center">-->
-        <!--                    <v-col>-->
-
-        <!--                        <c-pagination-iterator :paginator="places" item-key="id">-->
-        <!--                            <template v-slot:default="{item}">-->
-        <!--                                <c-place-card :place="item">-->
-        <!--                                    <template v-slot:icons>-->
-        <!--                                        <v-tooltip bottom>-->
-        <!--                                            <template v-slot:activator="{ on, attrs }">-->
-        <!--                                                <v-btn-->
-        <!--                                                    icon-->
-        <!--                                                    @click="removeFromRoute(item)"-->
-        <!--                                                    v-bind="attrs"-->
-        <!--                                                    v-on="on"-->
-        <!--                                                >-->
-        <!--                                                    <v-icon>mdi-minus</v-icon>-->
-        <!--                                                </v-btn>-->
-        <!--                                            </template>-->
-        <!--                                            Remove from route-->
-        <!--                                        </v-tooltip>-->
-        <!--                                    </template>-->
-        <!--                                </c-place-card>-->
-        <!--                            </template>-->
-        <!--                        </c-pagination-iterator>-->
-        <!--                    </v-col>-->
-        <!--                </v-row>-->
-
-
-        <!--                <v-row-->
-        <!--                    align="center"-->
-        <!--                    justify="center">-->
-        <!--                    <v-col>-->
-        <!--                        <c-place-search ref="placeSearch" :route-id="routeModel.id" @addToRoute="addToRoute" title="Search for a place" button-text="Add to route">-->
-        <!--                            <template v-slot:activator="{trigger,showing}">-->
-        <!--                                <v-btn :disabled="showing" @click="trigger">-->
-        <!--                                    Find Places-->
-        <!--                                </v-btn>-->
-        <!--                            </template>-->
-        <!--                        </c-place-search>-->
-        <!--                    </v-col>-->
-        <!--                </v-row>-->
     </div>
 </template>
 
