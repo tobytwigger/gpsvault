5df963ece503bd051e8c5dcc14053010df7005d6
Toby Twigger <tobytwigger1@gmail.com>
2022-12-21 01:19:53 +0000

    Import required classes

--- app/Integrations/Strava/Import/Api/Resources/LimitedActivity.php
+++ app/Integrations/Strava/Import/Api/Resources/LimitedActivity.php
@@ -3,6 +3,10 @@
 namespace App\Integrations\Strava\Import\Api\Resources;
 
 use App\Integrations\Strava\Jobs\LoadStravaActivity;
+use App\Integrations\Strava\Jobs\LoadStravaComments;
+use App\Integrations\Strava\Jobs\LoadStravaKudos;
+use App\Integrations\Strava\Jobs\LoadStravaPhotos;
+use App\Integrations\Strava\Jobs\LoadStravaStats;
 use App\Models\Activity as ActivityModel;
 use App\Models\Stats;
 use App\Models\User;
